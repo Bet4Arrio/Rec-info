5f7caeb077bad5cc6fbf00421924113be7082bd1
Bet4Arrio <emanuelsa-rv@hotmail.com>
2024-06-10 13:03:59 -0300

    Feat: queries

--- main.go
+++ main.go
@@ -19,17 +19,11 @@ func main() {
 	corpos := core.CorpusDirFactory(a, ".txt")
 	fmt.Printf("%s took %v\n", "Corpus", time.Since(start))
 	corpos.PrintCorpus()
+	corpos.Show20()
 	start = time.Now()
 	sis := vetorial.VetorialFactory(corpos)
 	fmt.Printf("%s took %v\n", "Vetorial", time.Since(start))
 	fmt.Printf("%s took %v\n", "corpus+Vetorial", time.Since(startbase))
-	// start = time.Now()
-	// sis.Save()
-	// fmt.Printf("%s took %v\n", "save", time.Since(start))
-	// start = time.Now()
-
-	// sis2 := vetorial.LoadFromJson("Vetorial.json")
-	// fmt.Printf("%s took %v\n", "Load", time.Since(start))
 	var q string
 	in := bufio.NewReader(os.Stdin)
 	fmt.Print("Pesquisa: ")
@@ -37,18 +31,17 @@ func main() {
 	if err != nil {
 		panic("panic")
 	}
-	for q != "cu\n" {
+	for q != ":q\n" {
 		fmt.Println(q)
 		start = time.Now()
 		teste := sis.Performquery(q[:len(q)-1])
-		// teste2 := sis2.Performquery(q[:len(q)-1])
-		fmt.Println(len(teste), "Encontrado")
-		fmt.Print("Pesquisa: ")
+		fmt.Println(len(teste), "Encontrados")
 		fmt.Printf("%s took %v\n", "Vetorial Save", time.Since(start))
+		vetorial.PageQuery(teste)
+		fmt.Print("Nova Pesquisa: ")
 		q, err = in.ReadString('\n')
 		if err != nil {
 			panic("panic")
 		}
 	}
-
 }
