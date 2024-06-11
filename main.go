b32ffa637ade72a744b12fe94e137dc7d8e2b72d
Bet4Arrio <emanuelsa-rv@hotmail.com>
2024-06-11 12:44:22 -0300

    Feat: menu UwU

--- main.go
+++ main.go
@@ -16,16 +16,22 @@ func main() {
 	a := core.Path{Val: "./data/"}
 	start := time.Now()
 	startbase := time.Now()
+	fmt.Println("mounting Corpus")
 	corpos := core.CorpusDirFactory(a, ".txt")
 	fmt.Printf("%s levou %v\n", "Corpus", time.Since(start))
 	corpos.PrintCorpus()
 	corpos.Show20()
 	start = time.Now()
+	fmt.Println("mounting Vetorial Model")
+	fmt.Println("mounting Vetorial Model")
 	sis := vetorial.VetorialFactory(corpos)
 	fmt.Printf("%s levou %v\n", "Vetorial", time.Since(start))
 	fmt.Printf("%s levou %v\n", "corpus+Vetorial", time.Since(startbase))
 	var q string
 	in := bufio.NewReader(os.Stdin)
+	fmt.Println("===========================================================")
+	fmt.Println("VETORIAL SEATCH UwU")
+	fmt.Println("===========================================================")
 	fmt.Print("Pesquisa: ")
 	q, err := in.ReadString('\n')
 	if err != nil {
